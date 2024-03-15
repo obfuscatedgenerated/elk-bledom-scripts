ef8d4a8f9b796af7ef03762508fe94b51127ee3c
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-03-15 01:13:50 +0000

    use tweener

--- src/screen.py
+++ src/screen.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import asyncio
 
 from bledom.device import BleLedDevice
@@ -5,6 +7,8 @@ from bleak import BleakScanner, BleakClient
 
 from PIL import Image
 from mss import mss
+ 
+from tweener import Tween, Easing, EasingMode
 
 from dotenv import load_dotenv
 from os import getenv
@@ -14,18 +18,21 @@ load_dotenv()
 TARGET_DEVICE = getenv("TARGET_DEVICE")
 
 # the required change in distance between the current color and the target color to trigger an update
-CHANGE_THRESHOLD = 5
+CHANGE_THRESHOLD = 10
 
-# the number of steps to take to get to the new color. more = smoother but slower. not much advantage to going over 255 TODO smoothing curves
-CHANGE_STEPS = 125
+# the time between each color change in milliseconds. smaller time = less smooth but more responsive
+TWEEN_TIME = 500
 
-# the time for each step in the color change
-CHANGE_RATE = 0.00001
+# the easing curve to use for the color change
+TWEEN_EASE = Easing.QUAD
+
+# the easing mode to use for the color change
+TWEEN_MODE = EasingMode.IN_OUT
 
 # TODO proper way to disable fading
 
-# the time between each screen capture
-UPDATE_RATE = 0.0015
+# the time between each screen capture in milliseconds
+UPDATE_RATE = int(1 / 60) * 1000
 
 # the initial color of the LED
 INIT_COLOR = (0, 0, 0)
@@ -54,38 +61,58 @@ if TARGET_DEVICE is None or TARGET_DEVICE == "":
 # TODO: move setup to common file
 # TODO: make library quiet
     
-current_color = INIT_COLOR
-target_color = INIT_COLOR
+
+current_color: Tuple[int, int, int] = INIT_COLOR
+target_color: Tuple[int, int, int] = INIT_COLOR
+
+tween_r: Tween | None = None
+tween_g: Tween | None = None
+tween_b: Tween | None = None
+# TODO: avoid flickering from multiple color updates at once (tweens are replaced immediately from current value)
+# especially from sudden changes. could also perhaps detect sudden changes and skip tweening optionally
 
 async def color_task(device):
     global current_color
     global target_color
 
+    global tween_r
+    global tween_g
+    global tween_b
+
     while True:
-        await asyncio.sleep(CHANGE_RATE)
+        # allow other tasks to run
+        await asyncio.sleep(0)
 
-        # get the delta between the current color and the target color
-        dr = target_color[0] - current_color[0]
-        dg = target_color[1] - current_color[1]
-        db = target_color[2] - current_color[2]
+        if tween_r is None or tween_g is None or tween_b is None:
+            continue
 
-        # interpolate the color
-        new_r = current_color[0] + dr / CHANGE_STEPS
-        new_g = current_color[1] + dg / CHANGE_STEPS
-        new_b = current_color[2] + db / CHANGE_STEPS
+        if tween_r.animating:
+            tween_r.update()
+        if tween_g.animating:
+            tween_g.update()
+        if tween_b.animating:
+            tween_b.update()
 
-        current_color = (new_r, new_g, new_b)
-        rounded_color = (int(new_r), int(new_g), int(new_b))
+        current_color = (int(tween_r.value), int(tween_g.value), int(tween_b.value))
 
-        # TODO: implement threshold. distance calc is weird
+        if current_color == target_color:
+            continue
 
-        asyncio.create_task(device.set_color(*rounded_color)) # run as task (without awaiting), means we can just skip a step if there is latency
+        asyncio.create_task(device.set_color(*current_color))
 
 async def screen_task():
+    global current_color
     global target_color
 
-    while True:
-        with mss() as sct:
+    global tween_r
+    global tween_g
+    global tween_b
+
+
+    with mss() as sct:
+        while True:
+            await asyncio.sleep(UPDATE_RATE)
+
             grab = sct.grab(sct.monitors[MONITOR_INDEX])
             im = Image.frombytes("RGB", grab.size, grab.bgra, "raw", "BGRX")
 
@@ -106,9 +133,20 @@ async def screen_task():
             g = int(g / len(pixels))
             b = int(b / len(pixels))
 
+            # if the color hasn't changed enough, don't update the target color
+            if abs(r - target_color[0]) <= CHANGE_THRESHOLD and abs(g - target_color[1]) <= CHANGE_THRESHOLD and abs(b - target_color[2]) <= CHANGE_THRESHOLD:
+                continue
+
             target_color = (r, g, b)
 
-            await asyncio.sleep(UPDATE_RATE)
+            tween_r = Tween(current_color[0], target_color[0], TWEEN_TIME, TWEEN_EASE, TWEEN_MODE)
+            tween_g = Tween(current_color[1], target_color[1], TWEEN_TIME, TWEEN_EASE, TWEEN_MODE)
+            tween_b = Tween(current_color[2], target_color[2], TWEEN_TIME, TWEEN_EASE, TWEEN_MODE)
+
+            tween_r.start()
+            tween_g.start()
+            tween_b.start()
+            
 
 async def main():
     print("Connecting to device...")
@@ -129,3 +167,4 @@ async def main():
 asyncio.run(main())
 
 # TODO: optionally prefer edges of screen
+# TODO: gui for settings
